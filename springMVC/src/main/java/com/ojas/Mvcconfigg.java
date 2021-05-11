06a6e4b0f97251f40db83d0eba0608fe09d45fc7
anureddy syamakuri <anureddy syamakuri@host.docker.internal>
2021-05-11 14:46:02 +0530

    SpringMVC

---
+++ springMVC/src/main/java/com/ojas/Mvcconfigg.java
@@ -0,0 +1,20 @@
+package com.ojas;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+ 
+
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class Mvcconfigg extends  WebMvcConfigurerAdapter {
+
+ 
+
+}
+	
+
+
